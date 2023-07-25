5365aa731742273150479a51738f5f498965ee86
fallennyx <awesomelekan@gmail.com>
2023-07-25 04:59:11 -0400

    AJAX POST WORKED FROM WEATHER, simplfiied views.py to app.py and deleted views.

--- app.py
+++ app.py
@@ -1,8 +1,50 @@
-from flask import Flask
-from views import views
-app = Flask(__name__)
+import os
 
+from flask_cors import CORS
+
+from data import get_outfits, get_forecast
+from flask import Blueprint, render_template, session, request, jsonify, Flask
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+# Define the views blueprint
+views = Blueprint(__name__, "views")
+
+@views.route("/")
+def home():
+    return render_template("home.html")
+
+@views.route("/generator")
+def generator():
+    return render_template("generator.html")
+
+@views.route("/style")
+def style():
+    return render_template("style.html")
+
+@views.route("/results")
+def results():
+    return render_template("results.html")
+
+
+
+@views.route("/x", methods=["GET", "POST"])
+def get_data_from_results():
+    if request.method == "POST":
+        try:
+            data = request.get_json()  # Use request.get_json() to parse JSON data
+
+            city = data.get("city")
+            gender = data.get("gender")
+
+            forecast = get_forecast(city)
+
+            return (forecast)
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__':
-    app.run(debug=True,  host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
